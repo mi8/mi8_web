cb5782c8b925698e32318054bf707074758a1829
Kevin Nguyen <kaxuo@hotmail.com>
2022-02-11 14:45:31 +0100

    footer started, homepage logo changed , cleanup

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,17 +4,19 @@ import Header from './Header';
 import Footer from './Footer';
 import SubFooter from './SubFooter';
 import '../scss/style.scss';
+import Mi8footer from './mi8Footer';
 
-const Layout = props => (
+const Layout = (props) => (
   <>
     <SEO />
     <div className={`page${props.bodyClass ? ` ${props.bodyClass}` : ''}`}>
-      <div id="wrapper" className="wrapper">
+      <div id='wrapper' className='wrapper'>
         <Header />
         {props.children}
       </div>
       <Footer />
       <SubFooter />
+      <Mi8footer />
     </div>
   </>
 );
---
+++ src/components/mi8Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../scss/components/_mi8Footer.scss';
+
+const Mi8footer = () => {
+  return (
+    <>
+      <h2 className='mi8'>Footer in construction</h2>
+    </>
+  );
+};
+
+export default Mi8footer;
